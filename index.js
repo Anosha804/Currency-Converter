27422d1012225c5da3ba22599e2e60db9cf355f9
Anosha Fatima <162595951+Anosha804@users.noreply.github.com>
2024-04-18 10:30:44 -0700

    Push my app to GitHub

---
+++ index.js
@@ -0,0 +1,42 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.blue.bold("\n \t My Currency Converter Application \n"));
+// Define the list of currencies and their exchange rates
+let exchangeRate = {
+    "USD": 1, // Base Currency
+    "EUR": 0.9,
+    "JYP": 113,
+    "CAD": 1.3,
+    "AUD": 1.65,
+    "PKR": 260,
+};
+// Prompt the user to select currencies to convert from and to
+let user_ans = await inquirer.prompt([
+    {
+        name: "from_currency",
+        type: "list",
+        message: "Select the currency to convert from",
+        choices: ["USD", "EUR", "JYP", "CAD", "AUD", "PKR"]
+    },
+    {
+        name: "to_currency",
+        type: "list",
+        message: "Select the currency to convert into",
+        choices: ["USD", "EUR", "JYP", "CAD", "AUD", "PKR"]
+    },
+    {
+        name: "amount",
+        type: "input",
+        message: "Enter the amount to convert: "
+    }
+]);
+// Perform currency conversion by using formula
+let from_amount = exchangeRate[user_ans.from_currency];
+let to_amount = exchangeRate[user_ans.to_currency];
+let amount = user_ans.amount;
+// Formula of conversion
+let base_amount = amount / from_amount;
+let converted_amount = base_amount * to_amount;
+// Display the converted amount
+console.log(`Converted amount = ${chalk.green(converted_amount.toFixed(2))}`);
